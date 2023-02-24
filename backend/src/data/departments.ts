7e590210d988693d4cfbaa6aa7fcf6a59e892fc9
pinkhairs <me@diana.nu>
2023-02-23 17:50:48 -0700

    separate data for reusability

---
+++ backend/src/data/departments.ts
@@ -0,0 +1,33 @@
+import Employees from "./employees"
+
+const getDeptEmployees = (id: string) => {
+    return Employees.filter((employee) => {
+        return employee.departmentId === id
+    })
+}
+
+const departments = [
+    {
+        id: "2",
+        name: "Full Stack Development",
+        get employees() {
+            return getDeptEmployees(this.id)
+        }
+    },
+    {
+        id: "3",
+        name: "Connected Devices Engineering",
+        get employees() {
+            return getDeptEmployees(this.id)
+        }
+    },
+    {
+        id: "4",
+        name: "Android Engineering",
+        get employees() {
+            return getDeptEmployees(this.id)
+        }
+    },
+]
+
+export default departments
---
+++ backend/src/data/employees.ts
@@ -0,0 +1,39 @@
+const employees = [
+    {
+        id: "82837",
+        name: "Bob Smith",
+        birthday: new Date("1985-09-27"),
+        bio: "Bob has been programming computers for entirely too long!",
+        departmentId: "3"
+    },
+    {
+        id: "81832",
+        name: "Ada Burr",
+        birthday: new Date("1985-10-18"),
+        bio: "Ada loves full stack development.",
+        departmentId: "4"
+    },
+    {
+        id: "82338", // changed to string
+        name: "Molly Davis",
+        birthday: new Date("1985-09-27"),
+        bio: "Molly once found a bug in a compiler",
+        departmentId: "2"
+    },
+    {
+        id: "32673",
+        name: "François Allende",
+        birthday: new Date("1985-09-27"),
+        bio: "François is the best QA engineer West of the Susquehanna river.",
+        departmentId: "2"
+    },
+    {
+        id: "zc",
+        name: "Juan Cortez",
+        birthday: '1984-09-22',
+        bio: "Juan's been programming computers since the days of ATARI BASIC.",
+        departmentId: "4"
+    }
+]
+
+export default employees
